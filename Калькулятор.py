2d3f430e525ac38ddba0b90095718c4c659b2b11
ashot15 <38916348+ashot15@users.noreply.github.com>
2023-11-06 11:22:19 +0500

    Add files via upload
    Good

--- Калькулятор.py
+++ Калькулятор.py
@@ -1,21 +1,15 @@
-
-
-
 Number1 = int(input("First Number :"))
 Number2 = int(input("Second Number :"))
 char = (input("Character :"))
-
 if char == "*":
     Result = Number1 * Number2
 elif char == "+":
      Result =  Number1 + Number2
 elif char == "/":
     Result = Number1 / Number2
-
 else:
     char == "-"
     Result = Number1 - Number2
-    
 print(Result)
     
 
