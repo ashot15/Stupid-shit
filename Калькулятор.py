3fb86408260385b519f5149dd75095257763f43b
ashot15 <38916348+ashot15@users.noreply.github.com>
2023-11-06 11:15:36 +0500

    Add files via upload

---
+++ Калькулятор.py
@@ -0,0 +1,22 @@
+
+
+
+Number1 = int(input("First Number :"))
+Number2 = int(input("Second Number :"))
+char = (input("Character :"))
+
+if char == "*":
+    Result = Number1 * Number2
+elif char == "+":
+     Result =  Number1 * Number2
+elif char == "/":
+    Result = Number1 / Number2
+
+else:
+    char == "-"
+    Result = Number1 - Number2
+    
+print(Result)
+    
+
+    
