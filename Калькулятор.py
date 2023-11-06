645d36a32e342e55dd5edaff9560309dcb9e4008
ashot15 <38916348+ashot15@users.noreply.github.com>
2023-11-06 12:44:00 +0500

    Add files via upload

--- Калькулятор.py
+++ Калькулятор.py
@@ -1,16 +1,25 @@
-Number1 = int(input("First Number :"))
-Number2 = int(input("Second Number :"))
-char = (input("Character :"))
-if char == "*":
-    Result = Number1 * Number2
-elif char == "+":
-     Result =  Number1 + Number2
-elif char == "/":
-    Result = Number1 / Number2
-else:
-    char == "-"
-    Result = Number1 - Number2
-print(Result)
-    
+import time as t
 
-    
+
+try:
+    Number1 = int(input("First Number: "))
+    Number2 = int(input("Second Number: "))
+    char = input("Character: ")
+
+    if char == "+":
+        Result = Number1 + Number2
+    elif char == "*":
+        Result = Number1 * Number2
+    elif char == "/":
+        Result = Number1 / Number2
+    elif char == "-":
+        Result = Number1 - Number2
+    else:
+        Result = "Invalid operator"
+
+    print (Result)
+except ValueError:
+    print("Please write numbers")
+
+t.sleep(1.5)
+exit()
